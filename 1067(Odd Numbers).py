ca84b7c775861de2368b98bc60c8f2cf22725558
Rayhan Uddin <63502199+Rayhan-Uddin@users.noreply.github.com>
2023-01-28 03:06:56 +0600

    Add files via upload

---
+++ 1067(Odd Numbers).py
@@ -0,0 +1,4 @@
+m = int(input())
+for i in range(m+1):
+    if(i%2==1):
+        print(i)
---
+++ 1070 (Six Odd Numbers).py
@@ -0,0 +1,7 @@
+a=int(input())
+i=0
+while(i<6):
+    if(a%2!=0):
+        print(n)
+        i=i+1
+    a=a+1
